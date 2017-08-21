836f5d1b4ab9151945bef52a3201118810b0ff95
dapobelieve <dapomichaels@gmail.com>
2017-08-21 09:17:13 -0400

    added email to users migration

--- database/migrations/2017_08_21_063759_create_users_table.php
+++ database/migrations/2017_08_21_063759_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('email');
         });
     }
 

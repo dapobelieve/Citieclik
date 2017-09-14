1452a41f22d1f698df67f101f767e41fbf705d63
Jeremiah <jerexbambex@gmail.com>
2017-09-14 11:14:02 +0100

    another change

--- resources/views/pages/addservice.blade.php
+++ resources/views/pages/addservice.blade.php
@@ -38,11 +38,11 @@ Add service | Citieclik
           </div>
         </div>
       </div>
-	<!-- Page Content-->
-      <div class="container padding-bottom-3x mb-2">
+		<!-- Page Content-->
+      	<div class="container padding-bottom-3x mb-2">
 	        <div class="row">
 	          <!-- Checkout Adress-->
-	          <div class="col-xl-9 col-lg-8">
+	          	<div class="col-xl-9 col-lg-8">
 		          {{-- @if(count($errors) > 0)
 		            <div class="alert alert-danger alert-dismissible fade show margin-bottom-1x">
 			            <span class="alert-close" data-dismiss="alert"></span><i class="icon-ban"></i>&nbsp;&nbsp;
@@ -113,10 +113,10 @@ Add service | Citieclik
 			              	<div class="col-sm-6">
 				                <div class="form-group {{ $errors->has('location') ? ' has-error' : '' }}">
 				                  	<label for="checkout-country">Location</label>
-					                  <select class="form-control" name="location" disabled id="location" value="{{ Request::old('location') ?: ''  }}">
+					                <select class="form-control" name="location" disabled id="location" value="{{ Request::old('location') ?: ''  }}">
 					                    <option>Choose Location</option>
 					                    
-					                  </select>
+					                </select>
 			                  		@if ($errors->has('location'))
 										<p class="help-block text-danger"><i class="icon-circle-cross"></i>&nbsp;{{ $errors->first('location') }}</p>
 				                	@endif
@@ -160,76 +160,76 @@ Add service | Citieclik
 			            </div>
 			            {{csrf_field()}}
 			        </form>
-	          </div>
-	          <!-- Sidebar          -->
-	          <div class="col-xl-3 col-lg-4">
-	            <aside class="sidebar">
-	              <div class="padding-top-2x hidden-lg-up"></div>
-	              <!-- Order Summary Widget-->
-	              <section class="widget widget-order-summary">
-	                <h3 class="widget-title">Order Summary</h3>
-	                <table class="table">
-	                  <tr>
-	                    <td>Cart Subtotal:</td>
-	                    <td class="text-medium">$289.68</td>
-	                  </tr>
-	                  <tr>
-	                    <td>Shipping:</td>
-	                    <td class="text-medium">$22.50</td>
-	                  </tr>
-	                  <tr>
-	                    <td>Estimated tax:</td>
-	                    <td class="text-medium">$3.42</td>
-	                  </tr>
-	                  <tr>
-	                    <td></td>
-	                    <td class="text-lg text-medium">$315.60</td>
-	                  </tr>
-	                </table>
-	              </section>
-	              <!-- Featured Products Widget-->
-	              <section class="widget widget-featured-products">
-	                <h3 class="widget-title">Recently Viewed</h3>
-	                <!-- Entry-->
-	                <div class="entry">
-	                  <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/01.jpg" alt="Product"></a></div>
-	                  <div class="entry-content">
-	                    <h4 class="entry-title"><a href="shop-single.html">Oakley Kickback</a></h4><span class="entry-meta">$155.00</span>
-	                  </div>
-	                </div>
-	                <!-- Entry-->
-	                <div class="entry">
-	                  <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/02.jpg" alt="Product"></a></div>
-	                  <div class="entry-content">
-	                    <h4 class="entry-title"><a href="shop-single.html">Top-Sider Fathom</a></h4><span class="entry-meta">$90.00</span>
-	                  </div>
-	                </div>
-	                <!-- Entry-->
-	                <div class="entry">
-	                  <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/03.jpg" alt="Product"></a></div>
-	                  <div class="entry-content">
-	                    <h4 class="entry-title"><a href="shop-single.html">Vented Straw Fedora</a></h4><span class="entry-meta">$49.50</span>
-	                  </div>
-	                </div>
-	                <!-- Entry-->
-	                <div class="entry">
-	                  <div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/04.jpg" alt="Product"></a></div>
-	                  <div class="entry-content">
-	                    <h4 class="entry-title"><a href="shop-single.html">Big Wordmark Tote</a></h4><span class="entry-meta">$29.99</span>
-	                  </div>
-	                </div>
-	              </section>
-	              <!-- Promo Banner-->
-	              <section class="promo-box" style="background-image: url(img/banners/02.jpg);"><span class="overlay-dark" style="opacity: .4;"></span>
-	                <div class="promo-box-content text-center padding-top-2x padding-bottom-2x">
-	                  <h4 class="text-light text-thin text-shadow">New Collection of</h4>
-	                  <h3 class="text-bold text-light text-shadow">Sunglasses</h3><a class="btn btn-outline-white btn-sm" href="shop-grid-ls.html">Shop Now</a>
-	                </div>
-	              </section>
-	            </aside>
-	          </div>
+	          	</div>
+	          	<!-- Sidebar          -->
+	          	<div class="col-xl-3 col-lg-4">
+		            <aside class="sidebar">
+		              	<div class="padding-top-2x hidden-lg-up"></div>
+		              	<!-- Order Summary Widget-->
+		              	<section class="widget widget-order-summary">
+		                	<h3 class="widget-title">Order Summary</h3>
+		                	<table class="table">
+				                <tr>
+				                    <td>Cart Subtotal:</td>
+				                    <td class="text-medium">$289.68</td>
+				                </tr>
+				                <tr>
+				                    <td>Shipping:</td>
+				                    <td class="text-medium">$22.50</td>
+				                </tr>
+				                <tr>
+				                    <td>Estimated tax:</td>
+				                    <td class="text-medium">$3.42</td>
+				                </tr>
+				                <tr>
+				                    <td></td>
+				                    <td class="text-lg text-medium">$315.60</td>
+				                </tr>
+		                	</table>
+		              	</section>
+		              <!-- Featured Products Widget-->
+		              	<section class="widget widget-featured-products">
+		                	<h3 class="widget-title">Recently Viewed</h3>
+			                <!-- Entry-->
+			                <div class="entry">
+			                  	<div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/01.jpg" alt="Product"></a></div>
+			                  	<div class="entry-content">
+			                    	<h4 class="entry-title"><a href="shop-single.html">Oakley Kickback</a></h4><span class="entry-meta">$155.00</span>
+			                  	</div>
+			                </div>
+		                	<!-- Entry-->
+			                <div class="entry">
+			                  	<div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/02.jpg" alt="Product"></a></div>
+			                  	<div class="entry-content">
+			                    	<h4 class="entry-title"><a href="shop-single.html">Top-Sider Fathom</a></h4><span class="entry-meta">$90.00</span>
+			                  	</div>
+			                </div>
+		                	<!-- Entry-->
+		                	<div class="entry">
+		                  		<div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/03.jpg" alt="Product"></a></div>
+		                  		<div class="entry-content">
+		                    		<h4 class="entry-title"><a href="shop-single.html">Vented Straw Fedora</a></h4><span class="entry-meta">$49.50</span>
+		                  		</div>
+		                	</div>
+		                	<!-- Entry-->
+		                	<div class="entry">
+		                  		<div class="entry-thumb"><a href="shop-single.html"><img src="img/shop/widget/04.jpg" alt="Product"></a></div>
+		                 		<div class="entry-content">
+		                    		<h4 class="entry-title"><a href="shop-single.html">Big Wordmark Tote</a></h4><span class="entry-meta">$29.99</span>
+		                  		</div>
+		                	</div>
+		              	</section>
+		              	<!-- Promo Banner-->
+		              	<section class="promo-box" style="background-image: url(img/banners/02.jpg);"><span class="overlay-dark" style="opacity: .4;"></span>
+		                	<div class="promo-box-content text-center padding-top-2x padding-bottom-2x">
+		                  		<h4 class="text-light text-thin text-shadow">New Collection of</h4>
+		                  		<h3 class="text-bold text-light text-shadow">Sunglasses</h3><a class="btn btn-outline-white btn-sm" href="shop-grid-ls.html">Shop Now</a>
+		                	</div>
+		              	</section>
+		            </aside>
+	          	</div>
 	        </div>
-      </div>
+      	</div>
   	<!--Page Content-->
 @endsection
 

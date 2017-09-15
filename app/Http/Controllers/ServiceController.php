cbef2afdb51788a9d974974f40379ccfc7e8a4ab
Jeremiah <jerexbambex@gmail.com>
2017-09-15 11:47:03 +0100

    I don't know

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -46,11 +46,7 @@ class ServiceController extends Controller
             'location'  => 'required|integer',
             'serCat'    => 'required|integer',
             'subCat'    => 'required|integer',
-<<<<<<< HEAD
             'serImg'    => "image|mimes:jpeg,jpg,png,bmp,svg|max:2048",
-=======
-            'serImg'    => 'image|mimes:jpeg,jpg,png,bmp,svg|max:2048',
->>>>>>> cf14923f039d1d4be41850dd86a20927145d6cf0
             // 'servicePrice' => "integer",
             'description'  => 'required|string'
         ], 
@@ -62,13 +58,9 @@ class ServiceController extends Controller
             'location.integer'     => 'Select the location',
             'description.required'  => 'Give a short description of the sevice',
             'serCat.required'       => 'Select a Category',
-<<<<<<< HEAD
-            // 'subCat.required'       => 'Select a Sub Category',
-=======
             'serCat.integer'       => 'Select a Category',
             'subCat.required'       => 'Select a Sub Category',
             'subCat.integer'       => 'Select a Sub Category',
->>>>>>> cf14923f039d1d4be41850dd86a20927145d6cf0
             // 'servicePrice.integer' => 'The price must be in digits e.g 50000',
             'serImg.mimes'          => 'The image must have jpeg, jpg or png format',
             'serImg.max'            => 'The Image is too large, It must not be more than 2MB',

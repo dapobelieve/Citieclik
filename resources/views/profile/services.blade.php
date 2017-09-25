e8736a60369a66574d3a81b7e30341233266e73f
dapobelieve <dapomichaels@gmail.com>
2017-09-25 12:42:28 -0400

    Restlyed 3dot menu

--- resources/views/profile/services.blade.php
+++ resources/views/profile/services.blade.php
@@ -9,114 +9,38 @@ Services | Citieclik
 <link rel="stylesheet" type="text/css" href="/assets/css/slimscroll.css">
 <script type="text/javascript" src="/assets/js/slim.js"></script>
  <style>
-                    *{margin: 0;padding:0px}
+    *{margin: 0;padding:0px}
 
-                    .header{
-                        width: 100%;
-                        background-color: #0d77b6 !important;
-                        height: 60px;
-                    }
+  
+    .showLeft{
+        text-shadow: none !important;
+        color:#fff !important;
+    }
 
-                    .showLeft{
-                        background-color: #0d77b6 !important;
-                        border:1px solid #0d77b6 !important;
-                        text-shadow: none !important;
-                        color:#fff !important;
-                        padding:10px;
-                    }
+    .icons li {
+        background: none repeat scroll 0 0 #b5a4a4;
+        height: 4px;
+        width: 4px;
+        line-height: 0;
+        list-style: none outside none;
+        margin-top: 3px;
+        vertical-align: top;
+        border-radius:50%;
+        pointer-events: none;
+    }
 
-                    .icons li {
-                        background: none repeat scroll 0 0 #fff;
-                        height: 7px;
-                        width: 7px;
-                        line-height: 0;
-                        list-style: none outside none;
-                        margin-right: 15px;
-                        margin-top: 3px;
-                        vertical-align: top;
-                        border-radius:50%;
-                        pointer-events: none;
-                    }
+    .btn-left, .btn-right {
+        position: absolute;
+    }
 
-                    .btn-left {
-                        left: 0.4em;
-                    }
-
-                    .btn-right {
-                        right: 0.4em;
-                    }
-
-                    .btn-left, .btn-right {
-                        position: absolute;
-                        /*top: 0.24em;*/
-                    }
-
-                    .dropbtn {
-                        background-color: #4CAF50;
-                        /*position: fixed;*/
-                        color: white;
-                        font-size: 16px;
-                        border: none;
-                        cursor: pointer;
-                    }
-
-                    .dropbtn:hover, .dropbtn:focus {
-                        background-color: #3e8e41;
-                    }
-
-                    /*.dropdown {
-                        position: absolute;
-                        display: inline-block;
-                        right: 0.4em;
-                    }*/
-
-                    .dropdown-content {
-                        display: none;
-                        position: relative;
-                        margin-top: 60px;
-                        background-color: #f9f9f9;
-                        min-width: 160px;
-                        overflow: auto;
-                        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                        z-index: 1;
-                    }
-
-                    .dropdown-content a {
-                        color: black;
-                        padding: 12px 16px;
-                        text-decoration: none;
-                        display: block;
-                    }
-
-                    .dropdown a:hover {background-color: #f1f1f1}
-
-                    .show {display:block;}
+    .dropbtn {
+        color: white;
+        font-size: 16px;
+        border: none;*/
+        cursor: pointer;
+    }
+    .show {display:block;}
 </style>
-<script>
-                    function changeLanguage(language) {
-                        var element = document.getElementById("url");
-                        element.value = language;
-                        element.innerHTML = language;
-                    }
-
-                    function showDropdown() {
-                        document.getElementById("myDropdown").classList.toggle("show");
-                    }
-
-                    // Close the dropdown if the user clicks outside of it
-                    window.onclick = function(event) {
-                        if (!event.target.matches('.dropbtn')) {
-                            var dropdowns = document.getElementsByClassName("dropdown-content");
-                            var i;
-                            for (i = 0; i < dropdowns.length; i++) {
-                                var openDropdown = dropdowns[i];
-                                if (openDropdown.classList.contains('show')) {
-                                    openDropdown.classList.remove('show');
-                                }
-                            }
-                        }
-                    }
-</script>
 @stop
 
 
@@ -135,58 +59,48 @@ Services | Citieclik
                   </thead>
                 </table>
               <div class="slimScroll">
-                <table class="table">
-                  <tbody>
-                  
-                  @foreach($user->getUserServices() as $servy) 
-                    <tr class="justify-content-end">
-                      <td>
-                        <div class="product-item"><a class="product-thumb" href="shop-single.html"><img src="/assets/img/shop/cart/01.jpg" alt="Product"></a>
-                          <div class="product-info">
-                            <h4 class="product-title"><a href="shop-single.html">{{$servy->title}}</a></h4>
-                            <div class="text-lg text-medium text-muted">Category: {{$servy->catty->category}}</div>
-                            <div>Posted:
-                              <div class="d-inline text-success">{{$servy->created_at->diffForHumans()}}</div>
+                <div class="container">
+                    @foreach($user->getUserServices() as $servy) 
+                        <div class="row">
+                            <div class="col-sm-6 col-md-9">
+                                <div class="product-item"><a class="product-thumb" href="shop-single.html">
+                                    <img src="/assets/img/shop/cart/01.jpg" alt="Product"></a>
+                                      <div class="product-info">
+                                        <h4 class="product-title"><a href="shop-single.html">{{$servy->title}}</a></h4>
+                                        <div class="text-lg text-medium text-muted">Category: {{$servy->catty->category}}</div>
+                                        <div>Posted:
+                                          <div class="d-inline text-success">{{$servy->created_at->diffForHumans()}}</div>
+                                        </div>
+                                      </div>
+                                </div>
                             </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="">
-                        {{-- <a class="remove-from-cart" href="#" data-toggle="tooltip" title="Remove item"><i class="icon-cross"></i></a> --}}
-{{-- <<<<<<< HEAD --}}
-                        @if(Auth::check() && Auth::user()->id == $user->id)
-                        <span class="dropdown">
-                        <!-- three dots -->
-                        <ul class="dropbtn icons btn-right showLeft" onclick="showDropdown()">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                        <!-- menu -->
-                        <span id="myDropdown" class="dropdown-content">
-                            <a href="#home">Home</a>
-                            <a href="#about">About</a>
-                            <a href="#contact">Contact</a>
-                        </span>
-                    </span>
-                        @endif
-{{-- =======
-                        <div class="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <a class="" href="#"><i class="icon-ellipsis"></i></a>
-                          <div class="dropdown-menu mybox" aria-labelledby="dropdownMenuButton dropdown-menu-left">
-                            <a class="dropdown-item text-primary" href="#"><i class="icon-cog"></i> Edit</a>
-                            <a class="dropdown-item text-danger" href="#"><i class="icon-cross"></i>Delete</a>
-                          </div>
-                        </div>
->>>>>>> de29750514713e612da6037312423181c7dc96fa --}}
-                      </td>
-                      <td></td>
-                      <td></td>
-                    </tr>
+                            <div class="col-sm-3 col-md-3" style="padding-left:100px">
+                                 @if(Auth::check() && Auth::user()->id == $user->id)
+                                    <span class="dropdown">
+                                        <!-- three dots -->
+                                        <ul style="cursor:pointer" class="dropbtn icons btn-right showLeft" >
+                                            <li></li>
+                                            <li></li>
+                                            <li></li>
+                                        </ul>
+                                         <div class="dropdown-menu  mybox" style="min-width:0rem; margin-top 12rem"  aria-labelledby="dropdownMenuButton dropdown-menu-left">
+                                         <form method="GET" action="{{route('service.edit', $servy->id)}}">
+                                             <input class="dropdown-item text-primary" style="cursor:pointer" type="submit" value="Edit">
+                                             {{csrf_field()}}
+                                         </form>
+                                         <hr>
+                                         <form method="POST" action="{{route('service.delete', $servy->id)}}">
+                                             <input class="dropdown-item text-danger" style="cursor:pointer" type="submit" value="Delete">
+                                             {{method_field('DELETE')}}
+                                             {{csrf_field()}}
+                                         </form>
+                                        </div>
+                                    </span>
+                                @endif
+                            </div>
+                        </div> 
                     @endforeach
-                  
-                  </tbody>
-                </table>
+                  </div>
                 </div>
               </div>
               <hr class="mb-4">

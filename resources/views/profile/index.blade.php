8e08c39e34889f8151932faa8200c4c62dbfc5ab
dapoBelieve <dapomichaels@gmail.com>
2017-11-16 04:08:56 -0800

    minor changes

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -12,7 +12,7 @@ Edit | Profile
                   <div class="alert alert-success alert-dismissible fade show text-center margin-bottom-1x">
                     <span class="alert-close" data-dismiss="alert"></span>
                     <i class="icon-help"></i>&nbsp;&nbsp;
-                    <strong>Success alert:</strong>{{Session::get('info')}}
+                    <strong>Success alert: </strong>{{Session::get('info')}}
                   </div>
                   @endif
             

98ccedac3a04893988bbcba71e62d40aa92d31b2
dapoBelieve <dapomichaels@gmail.com>
2017-12-12 09:51:08 +0100

    UserServiceProvider

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\UserServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         JD\Cloudder\CloudderServiceProvider::class,

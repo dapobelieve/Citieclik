cb9b9c38622a10f296e3f7682de9587c398e5389
Jeremiah <jerexbambex@gmail.com>
2018-01-25 18:37:14 +0100

    Terms and conditions page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -4,6 +4,98 @@
 Citieclik | Terms and Condition
 @endsection
 
+@section('style')
+<style type="text/css">
+	body {
+		font-family: 'Proxima';
+		font-size: 1rem;
+		color: hsl(0, 0, 37%);
+		line-height: 1.5;
+		margin-bottom: 2.25rem;
+	}
+	p {
+		font-size: 1rem !important;
+		/*text-align: justify;*/
+	}
+	li {
+		line-height: 1.5;
+		margin-bottom: 2.25rem;
+	}
+	.m-head {
+		text-transform: uppercase !important;
+	}
+	.strong {
+		font-weight: bold !important;
+	}
+</style>
+@endsection
+
 @section('content')
-hello
+<!-- Off-Canvas Wrapper-->
+<div class="offcanvas-wrapper">
+    <!-- Page Title-->
+  	<div class="page-title">
+        <div class="container">
+          	<div class="column">
+            	<h1 class="strong" style="text-transform: uppercase;">Terms and Conditions</h1>
+          	</div>
+          	<div class="column">
+            	<ul class="breadcrumbs">
+	              	<li><a href="/">Home</a>
+	              	</li>
+	              	<li class="separator">&nbsp;</li>
+	              	<li>Terms and Conditions</li>
+            	</ul>
+          	</div>
+        </div>
+  	</div>
+  	<!-- Page Content-->
+  	<div class="container padding-bottom-3x mb-2">
+        <div class="row"> 
+          	<!-- Content-->
+          	<div class="col-lg-10 offset-lg-1">
+            	{{-- <h1 class="m-head strong">Terms and Conditions</h1><br> --}}
+            	<h2><strong>Citieclik Terms and Conditions / Privacy Policy</strong></h2>
+				<p>This Privacy Policy governs the manner in which Citieclik&nbsp;collects, uses, maintains and discloses information collected from users (each, a "User") of the www.citiclik.com&nbsp;website ("Site"). This privacy policy applies to the Site and all products and services offered by Citieclik.</p><br>
+				<h3><strong>Personal identification information</strong></h3>
+				<p>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, place an order, fill out a form, respond to a survey subscribe to the newsletter and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number, credit card information.</p>
+				<p>Users may, however, visit our Site anonymously.</p>
+				<p>We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+				<p>&nbsp;</p>
+				<h3><strong>Non-personal identification information</strong></h3>
+				<p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
+				<p>&nbsp;</p>
+				<h3><strong>Web browser cookies</strong></h3>
+				<p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+				<h3><strong>How we use collected information</strong></h3>
+				<p>Citieclik collects and uses Users personal information for the following purposes:</p>
+				<ul>
+					<li><strong><em>To improve customer service</em></strong><br /> Your information helps us to more effectively respond to your customer service requests and support needs.</li>
+					<li><strong><em>To personalize user experience</em></strong><br /> We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Site.</li>
+					<li><strong><em>To improve our Site</em></strong><br /> We continually strive to improve our website offerings based on the information and feedback we receive from you.</li>
+					<li><strong><em>To process transactions</em></strong><br /> We may use the information Users provide about themselves when placing an order only to provide service to that order. We do not share this information with outside parties except to the extent necessary to provide the service.</li>
+					<li><strong><em>To administer a content, promotion, survey or other Site feature</em></strong><br /> To send Users information they agreed to receive about topics we think will be of interest to them.</li>
+					<li><strong><em>To share your information with third parties</em></strong><br /> We may share or sell information with third parties for marketing or other purposes.</li>
+					<li><strong><em>To send periodic emails</em></strong><br /> The email address Users provide for order processing, will only be used to send them information and updates pertaining to their order. It may also be used to respond to their inquiries, and/or other requests or questions. If User decides to opt-in to our mailing list, they will receive emails that may include company news, updates, related product or service information, etc. If at any time the User would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email or User may contact us via our Site.</li>
+				</ul>
+				<p>&nbsp;</p>
+				<h3><strong>How we protect your information</strong></h3>
+				<p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
+				<p><br /> <br /> Sensitive and private data exchange between the Site and its Users happens over a SSL secured communication channel and is encrypted and protected with digital signatures.&nbsp;<br /> <br /> </p>
+				<h3><strong>Third party websites</strong></h3>
+				<p>Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website\'s own terms and policies.</p>
+				<h3><strong>Advertising</strong></h3>
+				<p>Ads appearing on our site may be delivered to Users by advertising partners, who may set cookies. These cookies allow the ad server to recognize your computer each time they send you an online advertisement to compile non personal identification information about you or others who use your computer. This information allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most interest to you. This privacy policy does not cover the use of cookies by any advertisers.</p>
+				<h3><strong>Google Adsense</strong></h3>
+				<p>Some of the ads may be served by Google. Google\'s use of the DART cookie enables it to serve ads to Users based on their visit to our Site and other sites on the Internet. DART uses "non-personally identifiable information" and does NOT track personal information about you, such as your name, email address, physical address, etc. You may opt out of the use of the DART cookie by visiting the Google ad and content network privacy policy at <a href="http://www.google.com/privacy_ads.html">http://www.google.com/privacy_ads.html</a></p>
+				<h3><strong>Compliance with children's online privacy protection act</strong></h3>
+				<p>Protecting the privacy of the very young is especially important. For that reason, we never collect or maintain information at our Site from those we actually know are under 13, and no part of our website is structured to attract anyone under 13.<br /> </p>
+				<h3><strong>Changes to this privacy policy</strong></h3>
+				<p>Citieclik has the discretion to update this privacy policy at any time. When we do, post a notification on the main page of our Site, revise the updated date at the bottom of this page, send you an email. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+				<h3><strong>Your acceptance of these terms</strong></h3>
+				<p>By using this Site, you signify your acceptance of this policy and&nbsp;<a href="http://www.citiclik.com/">terms of service</a>. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
+          	</div>
+        </div>
+  	</div>
+</div>
 @endsection

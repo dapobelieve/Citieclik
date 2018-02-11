55ffb37aed5c843a41af0ce29f8e88af172bd827
dapoBelieve <dapomichaels@gmail.com>
2018-02-11 09:47:34 +0100

    request listing done

--- resources/views/profile/requests.blade.php
+++ resources/views/profile/requests.blade.php
@@ -13,8 +13,8 @@ My Requests | Citieclik
             <thead>
               <tr>
                 <th>Request hey Name</th>
-                <th>Date Submitted | Updated</th>
-                <th>Type</th>
+                <th>Date Submitted</th>
+                {{-- <th>Type</th> --}}
                 <th>Priority</th>
                 {{-- <th>Status</th> --}}
               </tr>
@@ -23,8 +23,8 @@ My Requests | Citieclik
               @foreach($user->getUserReq() as $servy)
                 <tr>
                   <td><a class="text-medium navi-link" href="#">{{ $servy->title }}</a></td>
-                  <td>08/08/2017 | 08/14/2017</td>
-                  <td>Website problem</td>
+                  <td>{{ $servy->created_at->diffForHumans() }}</td>
+                  {{-- <td>Website problem</td> --}}
                   <td><span class="text-warning">{{ $servy->priority }}</span></td>
                   {{-- <td><span class="text-primary">Open</span></td> --}}
                 </tr>

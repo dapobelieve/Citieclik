d5b77bad3ad2fdf2c22ddb5fd8c038d095cf829e
dapoBelieve <dapomichaels@gmail.com>
2018-03-27 08:50:38 +0100

    artisan command

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Subscription;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'testing the work of this before going live with it';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}

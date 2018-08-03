b20847dcea4bd7d7f0bc7e22157a8acba988739a
dapobelieve <dredsn@gmail.com>
2018-08-03 11:49:43 +0100

    fixed search image display

--- resources/views/search/template/results.blade.php
+++ resources/views/search/template/results.blade.php
@@ -2,12 +2,14 @@
 	<div class="card mb-4">
       	<div class="card-header">Category: <span class="badge badge-pill badge-primary">{{$data->catty->category}}</span></div>
       	<div class="card-block">
-        	<div class="d-flex"><a class="pr-4 hidden-xs-down" href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}" style="max-width: 225px;"><img src={{$data->getImages()}} alt="Product" height="50" style="height: 150px !important;"></a>
+        	<div class="d-flex">
+                <a class="pr-4 hidden-xs-down" href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}" style="max-width: 225px;">
+                    {!! $data->getImages() !!} 
+                </a>
           		<div>
-            		<h5><a class="navi-link" href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}">{{$data->title}}</a></h5>
-		            <h6>
-		              {{-- <del class="text-muted">$99.99</del> $49.99 --}}
-		            </h6>
+            		<h5>
+                        <a class="navi-link" href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}">{{$data->title}}</a>
+                    </h5>
             		<div class="product-buttons">
 		              <a href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}" style="color: #0da9ef; text-transform: capitalize;" >Details</a>
 		            </div>

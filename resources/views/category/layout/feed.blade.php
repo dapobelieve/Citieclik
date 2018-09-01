1f56b2c0efdec9da17f0319cb2f4b4ce40a3fc38
dapobelieve <dredsn@gmail.com>
2018-09-01 21:12:18 +0100

    rest env debug

--- resources/views/category/layout/feed.blade.php
+++ resources/views/category/layout/feed.blade.php
@@ -4,43 +4,46 @@
 <div class="isotope-grid isodata cols-3 mb-2">
     <div class="gutter-sizer"></div>
     <div class="grid-sizer"></div>
-
-    @foreach($listings as $data)
-        <div class="isoitem grid-item 
-                    {{$data->slugIt($data->catty->slug)}} 
-                    {{$data->slugIt($data->loca->lga)}} 
-                    {{$data->slugIt($data->state->state)}}"
-        >
-            <div class="recent_ads_item">
-                <div class="item_image">
-                    <a class="img_wrapper" href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}">
-                        {!! $data->getImages() !!}
-                    </a>
-                </div>
-                <div class="item_details ">
-                    <div class="ser-title">
-                        <a href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}">
-                            {{ $data->serviceTitle() }}
+    @if ($listings->count() > 0)
+        @foreach($listings as $data)
+            <div class="isoitem grid-item 
+                        {{$data->slugIt($data->catty->slug)}} 
+                        {{$data->slugIt($data->loca->lga)}} 
+                        {{$data->slugIt($data->state->state)}}"
+            >
+                <div class="recent_ads_item">
+                    <div class="item_image">
+                        <a class="img_wrapper" href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}">
+                            {!! $data->getImages() !!}
                         </a>
                     </div>
-                    <div style="font-size: 15px" class="location">
-                        {{ $data->loca->lga }}
-                        {{-- {{ $data->created_at }} --}}
-                    </div>
-                    <div class="item_price">
-                        <span style="color: #2bd519" class="ser-price">
-                        @if(!$data->price == 0)
-                        <span> 
-                            &#x20A6</span>{{  number_format($data->price) }}
-                        </span>
-                        @else 
-                            Negotiable
-                        @endif
+                    <div class="item_details ">
+                        <div class="ser-title">
+                            <a href="{{route('service.detail',['username' => $data->userz->username,'slug' => $data->slug])}}">
+                                {{ $data->serviceTitle() }}
+                            </a>
+                        </div>
+                        <div style="font-size: 15px" class="location">
+                            {{ $data->loca->lga }}
+                            {{-- {{ $data->created_at }} --}}
+                        </div>
+                        <div class="item_price">
+                            <span style="color: #2bd519" class="ser-price">
+                            @if(!$data->price == 0)
+                            <span> 
+                                &#x20A6</span>{{  number_format($data->price) }}
+                            </span>
+                            @else 
+                                Negotiable
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    @else
+        <p>No listings yet</p>
+    @endif
 </div>
 {{-- <nav class="pagination"> --}}
         {{-- {{ $listings->render() }} --}}
